40c224fe4a6b09828b0ca22d0badbd418a0de0ae
EdwardW710 <EdwardW710@yahoo.com>
2025-04-09 14:52:12 -0400

    deployment set-up completed

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
 
+class Car:
+    def __init__(self, make, model, year, image):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.image = image 
+
+cars = [
+    Car('Dodge', 'Ram', '2024', 'No Current Image'),
+    Car('Chevorlet', 'Silverado', '2023', 'No Current Image'),
+    Car('Chevorlet', 'SS Impala', '1996', 'No Current Image')
+]
+        
+
 def home(request):
-    return HttpResponse('<h1>Hello Cars</h1>')
+    return render(request, 'home.html')
 
 def about(request):
-    return HttpResponse('<h1>About the CarColletor</h1>')
+    return render(request, 'about.html')
+
+def car_index(request):
+    return render(request, 'cars/index.html', {'cars': cars})
