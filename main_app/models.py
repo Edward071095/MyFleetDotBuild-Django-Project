a3e3413ebfc77198c1efaf043404fee431854a1a
EdwardW710 <EdwardW710@yahoo.com>
2025-04-09 17:24:45 -0400

    created models

--- main_app/models.py
+++ main_app/models.py
@@ -5,7 +5,7 @@ class Car(models.Model):
     make = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
     year = models.IntegerField()
-    image = models.ImageField()
+    image = models.ImageField(upload_to='car_images/', null=True, blank=True)
 
     def __str__(self):
         return f'{self.make} ({self.id})'
--- main_app/views.py
+++ main_app/views.py
@@ -1,22 +1,8 @@
 from django.shortcuts import render
+from .models import Car
 
 
 # Create your views here.
-
-class Car:
-    def __init__(self, make, model, year, image):
-        self.make = make
-        self.model = model
-        self.year = year
-        self.image = image 
-
-cars = [
-    Car('Dodge', 'Ram', '2024', 'No Current Image'),
-    Car('Chevorlet', 'Silverado', '2023', 'No Current Image'),
-    Car('Chevorlet', 'SS Impala', '1996', 'No Current Image')
-]
-        
-
 def home(request):
     return render(request, 'home.html')
 
@@ -24,4 +10,5 @@ def about(request):
     return render(request, 'about.html')
 
 def car_index(request):
+    cars = Car.objects.all()
     return render(request, 'cars/index.html', {'cars': cars})
